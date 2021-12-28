13a703cf9bdf463ede9c16bb04fead06347b770d
Javi Solis <javi@whykeyway.com>
2021-12-28 18:13:12 -0300

    change tests

--- src/test/kotlin/BirthdaysKataTest.kt
+++ src/test/kotlin/BirthdaysKataTest.kt
@@ -1,10 +1,18 @@
 import com.keyway.BirthdaysKata
+import com.keyway.domain.Employee
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class BirthdaysKataTest {
+
+    private val employees = listOf(
+        Employee("John", "john@keyway.com", "123, 5th Avenue", LocalDate.parse("1900-10-01", DateTimeFormatter.ISO_DATE)),
+        Employee("Kevin", "kevin@keyway.com", "183, Bird St", LocalDate.parse("1980-07-20", DateTimeFormatter.ISO_DATE)),
+        Employee("Robert", "rob@keyway.com", "725, Queens Ln", LocalDate.parse("1987-01-10", DateTimeFormatter.ISO_DATE)),
+        Employee("Monique", "monique@keyway.com", "898, Rocket Ln", LocalDate.parse("1983-03-01", DateTimeFormatter.ISO_DATE))
+    )
 
     @Test
     fun `given a date that matches with one employee birthday must send one greetings email`() {
